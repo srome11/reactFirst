6bde3d2bae60e4707d54069011f1c3a2f6cb48c2
Scott Rome <srome11@gmail.com>
2025-12-03 08:41:05 -0600

    Refactor App component to define stories within the component, pass them to List, and create a ListItem component for better structure and readability.

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.dev',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
 
-const App = () => ( 
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.dev',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
+
+  return (
     <div>
 
       <h1>Carthago delenda est</h1>
@@ -31,10 +34,11 @@ const App = () => (
 
       <hr />
       
-      <List />
+      <List list={stories}/>
 
     </div> 
-)
+  );
+}
 
 const Search = () => {
 
@@ -58,19 +62,25 @@ const Search = () => {
   ); 
 }
 
-const List = () => (
+const List = (props) => (
     <ul> 
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments} comments</span>
-          <span>{item.points} points</span>
-        </li>
+      {props.list.map((item) => (
+        <ListItem key={item.objectID} item={item} />
       ))}
     </ul>
+)
+
+const ListItem = (props) => (
+  <ul> 
+     <li key={props.item.objectID}>
+          <span>
+            <a href={props.item.url}>{props.item.title}</a>
+          </span>
+          <span>{props.item.author}</span>
+          <span>{props.item.num_comments} comments</span>
+          <span>{props.item.points} points</span>
+        </li>
+  </ul>
 )
 
 export default App
